b03d8d63ba75160fce5709bc6d31cffc02c721b8
Caron Baptiste <baptiste.caron@etu.univ-orleans.fr>
2023-09-13 09:42:37 +0200

    :lipstick: Couleurs et organisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import './styles/App.scss';
+import './styles/Effects.scss';
 import LangageSelector from "./components/LangageSelector";
 import {useTranslation} from "react-i18next";
 
@@ -12,11 +13,22 @@ function App() {
             const targetElement = document.getElementById(targetId || '');
 
             if (targetElement) {
+                const navbarElement = document.querySelector('.navbar') as HTMLElement;
+                const navbarHeight = navbarElement ? navbarElement.offsetHeight : 0;
+
                 window.scrollTo({
-                    top: targetElement.offsetTop,
+                    top: targetElement.offsetTop - navbarHeight,
                     behavior: 'smooth',
                 });
+                window.addEventListener("scroll", () => {
+                    if (window.scrollY > 50) {
+                        navbarElement.style.backgroundColor = "rgba(4, 6, 21, 0.5)"; // Fond avec fond flou
+                    } else {
+                        navbarElement.style.backgroundColor = "rgba(255, 255, 255, 0)"; // Fond transparent
+                    }
+                });
             }
+
         }
 
         // Attach event listeners to navbar links
@@ -31,8 +43,6 @@ function App() {
                 link.removeEventListener('click', handleSmoothScroll as unknown as EventListener);
             });
         };
-
-
     }, []);
 
     /* Fonction d'appel aux bases de langages */
@@ -41,6 +51,8 @@ function App() {
     return (
         <div className="App">
 
+
+            <header className="framer" data-framer-name="Header"></header>
             <div className="navbar">
                 <a href="#accueil">{t('accueil_libelle')}</a>
                 <a href="#presentation">{t('a_propos_libelle')}</a>
@@ -60,19 +72,18 @@ function App() {
                         />
                     </div>
                     <div className="intitulé">
-                        <p className="nom"> Baptiste CARON</p>
+                        <p className="nom">Baptiste CARON</p>
                         <p className="poste">{t('poste')}</p>
                     </div>
                 </section>
 
-                <section id="presentation" className="presentation">
-                    <div className="illustration">
-                        <img src="/to_fill.png" alt="to_fill"/>
-                    </div>
 
-                    <div className="texteSection">
-                        <h1 className="sectionTitle">{t('a_propos_libelle')}</h1>
-                        <p>{t('presentation')}</p>
+                <section id="presentation" className="presentation">
+                    <h1 className="sectionTitle">{t('a_propos_libelle')}</h1>
+                    <div className="texte">
+                        <div className="texteSection">
+                            <p>{t('presentation')}</p>
+                        </div>
                     </div>
 
                 </section>
@@ -80,69 +91,51 @@ function App() {
                 <section id="etudes" className="etudes">
                     <div className="texteSection">
                         <h1 className="sectionTitle">{t('parcours_scolaire_libelle')}</h1>
-                        <ul>
-                            <li>
-                                <label>{t('univ')}</label>
-                                <ul>
-                                    <li>{t('licence')}</li>
-                                    <li>{t('master')}</li>
-                                </ul>
-                            </li>
-
-                            <li>{t('bac')}</li>
-                        </ul>
+                        <div className="bloc">{t('licence')}</div>
+                        <div className="bloc">{t('master')}</div>
+                        <div className="bloc">{t('bac')}</div>
                     </div>
-                    <div className="illustration">
-                        <img src="/to_fill.png" alt="to_fill"/>
-                    </div>
-
                 </section>
 
                 <section id="parcoursPro" className="parcoursPro">
-                    <div className="illustration">
-                        <img src="/to_fill.png" alt="to_fill"/>
-                    </div>
-
                     <div className="texteSection">
                         <h1 className="sectionTitle">{t('experience_pro_libelle')}</h1>
-                        <ul>
-                            <li>
-                                <label>{t('sopra')}</label>
-                                <ul>
-                                    <li>{t('alt')}</li>
-                                    <li>{t('dev')}</li>
-                                </ul>
-                            </li>
-                        </ul>
+                        <div className="bloc">{t('alt')}</div>
+                        <div className="bloc">{t('dev')}</div>
                     </div>
                 </section>
 
                 <section id="competences" className="competences">
                     <div className="texteSection">
                         <h1 className="sectionTitle">{t('competences_libelle')}</h1>
-                        <ul>
-                            <li>{t('langagesProg')}</li>
-                            <li>{t('frameworkProg')}</li>
-                        </ul>
+                        <div className="bloc">
+                            <ul>
+                                <li>{t('java')}</li>
+                                <li>{t('python')}</li>
+                                <li>{t('ocaml')}</li>
+                                <li>{t('web')}</li>
+                                <li>{t('php')}</li>
+                                <li>{t('js')}</li>
+                                <li>{t('sql')}</li>
+                            </ul>
+                        </div>
+                        <div className="bloc">
+                            <ul>
+                                <li>{t('spring')}</li>
+                                <li>{t('react')}</li>
+                                <li>{t('angular')}</li>
+                                <li>{t('symphony')}</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="illustration">
-                        <img src="/to_fill.png" alt="to_fill"/>
-                    </div>
-
                 </section>
 
                 <section id="hobbies" className="hobbies">
-                    <div className="illustration">
-                        <img src="/to_fill.png" alt="to_fill"/>
-                    </div>
-
                     <div className="texteSection">
                         <h1 className="sectionTitle">{t('hobbies_libelle')}</h1>
-                        <ul>
-                            <li>{t('volley')}</li>
-                            <li>{t('astronomie')}</li>
-                            <li>{t('jeux_videos')}</li>
-                        </ul>
+                        <div className="bloc">{t('volley')}</div>
+                        <div className="bloc">{t('astronomie')}</div>
+                        <div className="bloc">{t('jeux_videos')}</div>
                     </div>
 
                 </section>
